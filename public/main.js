3e4915ac9482b453bcff7628b0300da7f9907b4e
Oana Donose <donoseo@coventry.ac.uk>
2020-11-22 21:53:09 +0000

    added visibility condition for msg

--- public/main.js
+++ public/main.js
@@ -1,3 +1,8 @@
-//window.addEventListener('DOMContentLoaded', () => {
-//	console.log('DOMContentLoaded')
-//})
+window.addEventListener('DOMContentLoaded', () => {
+	console.log('DOMContentLoaded')
+	const delay = 2000
+	document.querySelector('.msg').hidden = false
+	window.setTimeout(() => {
+		document.querySelector('.msg').hidden = true
+	}, delay)
+})
