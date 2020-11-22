77bef04e8dc2b7933b0b7e3c15c66c11902b1ece
Oana Donose <donoseo@coventry.ac.uk>
2020-11-22 22:18:09 +0000

    made leaderboards collapsible

--- public/main.js
+++ public/main.js
@@ -6,3 +6,17 @@ window.addEventListener('DOMContentLoaded', () => {
 		document.querySelector('.msg').hidden = true
 	}, delay)
 })
+
+const toggleLeaderboards = () => {
+	if (document.querySelector('.table-container').hidden) {
+		document.querySelector('.table-container').hidden = false
+	}
+	else {
+		document.querySelector('.table-container').hidden = true
+	}
+}
+
+const toggleBtn = document.querySelector('.toggle-btn')
+toggleBtn.addEventListener("click", toggleLeaderboards)
+
+
